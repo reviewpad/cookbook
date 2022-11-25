5f275cf5e1f17fabb2c89bec9107dd6d271c7f52
Zelalem Mekonen <zola@programmer.net>
2022-11-25 16:15:33 +0300

    feat: implement size recipe

--- recipes/size.go
+++ recipes/size.go
@@ -4,11 +4,93 @@
 
 package recipes
 
+import (
+	"context"
+
+	"github.com/dukex/mixpanel"
+	"github.com/reviewpad/cookbook/codehost"
+	"github.com/reviewpad/reviewpad/v3/handler"
+	"golang.org/x/exp/slices"
+)
+
 type Size struct {
-	Name string
+	targetEntity handler.TargetEntity
+	codehost     codehost.Codehost
+	collector    mixpanel.Mixpanel
 }
 
-func (s *Size) Run() error {
-	// TODO: implement
+func NewSizeRecipe(targetEntity handler.TargetEntity, codehost codehost.Codehost, collector mixpanel.Mixpanel) (*Size, error) {
+	return &Size{
+		targetEntity,
+		codehost,
+		collector,
+	}, nil
+}
+
+func (s *Size) Run(ctx context.Context) error {
+	owner := s.targetEntity.Owner
+	repo := s.targetEntity.Repo
+	number := s.targetEntity.Number
+
+	if err := createSizeLabels(ctx, owner, repo, s.codehost); err != nil {
+		return err
+	}
+
+	prSizeData, err := s.codehost.GetPRSizeData(ctx, owner, repo, number)
+	if err != nil {
+		return err
+	}
+
+	labelsToAdd := make([]string, 0)
+	labelsToRemove := make([]string, 0)
+
+	if prSizeData.Changes < 100 {
+		labelsToAdd = append(labelsToAdd, "small")
+		labelsToRemove = append(labelsToRemove, "medium", "large")
+	} else if prSizeData.Changes > 100 && prSizeData.Changes < 500 {
+		labelsToAdd = append(labelsToAdd, "medium")
+		labelsToRemove = append(labelsToRemove, "small", "large")
+	} else {
+		labelsToAdd = append(labelsToAdd, "large")
+		labelsToRemove = append(labelsToRemove, "small", "medium")
+	}
+
+	labels := append(prSizeData.Labels, labelsToAdd...)
+
+	for _, labelToRemove := range labelsToRemove {
+		index := slices.Index(labels, labelToRemove)
+
+		if index != -1 {
+			labels = slices.Delete(labels, index, index+1)
+		}
+	}
+
+	err = s.codehost.SetLabels(ctx, owner, repo, number, labels)
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func createSizeLabels(ctx context.Context, owner, repo string, ch codehost.Codehost) error {
+	labels := []codehost.Label{
+		{
+			Name:        "small",
+			Color:       "219ebc",
+			Description: "Pull request change is between 0 - 100 changes",
+		},
+		{
+			Name:        "medium",
+			Color:       "faedcd",
+			Description: "Pull request change is between 101 - 500 changes",
+		},
+		{
+			Name:        "large",
+			Color:       "e76f51",
+			Description: "Pull request change is more than 501+ changes",
+		},
+	}
+
+	return ch.CreateLabels(ctx, owner, repo, labels)
 }
