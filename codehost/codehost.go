c721b6438c23b1b71e7422f240f81c0c69c07769
Zelalem Mekonen <zola@programmer.net>
2022-11-26 17:09:19 +0300

    docs: add copyright notices

--- codehost/codehost.go
+++ codehost/codehost.go
@@ -1,3 +1,7 @@
+// Copyright (C) 2022 Explore.dev, Unipessoal Lda - All Rights Reserved
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
 package codehost
 
 import (
